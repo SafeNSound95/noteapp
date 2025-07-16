c2674dce15731eafac1c22ac6a1342e6abb8b6c2
Saif Shalan <safensound95@gmail.com>
2025-07-17 00:48:55 +0300

    wrote tests for user, more work on controller, model, middleware

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -2,12 +2,16 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find({})
+  response.json(users)
+})
+
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
 
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(password, saltRounds)
-  console.log('passhash:',passwordHash)
 
   const user = new User({
     username,
